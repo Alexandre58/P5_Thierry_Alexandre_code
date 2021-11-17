7b4310ce7f0e874c66aa221650dd574b4a8ed916
cherokee <cherokee.al58@outlook.com>
2021-11-17 13:45:59 +0100

    amélioration script.js

--- front/js/Modelkanap.js
+++ front/js/Modelkanap.js
@@ -9,94 +9,20 @@ export class Kanap {
         this.price = jsonArticle.price;
     }
     get getHtmlRepresentationDisplay() {
-        return this.getHtmlRepresentation()
+        return this.getHtmlRepresentation();
     }
     getHtmlRepresentation() {
-        return       
-        `   <a href="/front/html/product.html?id=${this._id}">
-                <article>
-                    <img src="${this.imageUrl}" alt="${this.altTxt}" width="160" height="160">
-                    <h3 class="productName">${this.name}</h3>
-                    <p class="productDescription">${this.description}</p>
-                </article>
-            </a>
-      `;
+                return  `   <a href="/front/html/product.html?id=${this._id}">
+                                <article>
+                                <img src="${this.imageUrl}" alt="${this.altTxt}" width="160" height="160">
+                                <h3 class="productName">${this.name}</h3>
+                                <p class="productDescription">${this.description}</p>
+                                </article>
+                            </a>
+                            `;
     }
   }; 
 
-/*export class Kanap {
-    constructor(productList){
-        productList && Object.assign(this,productList);
-    }*/
- /*
-get id () {
-
-}
-
-set id() {
-
-}
-
-set prix (newPrix) {
-    if (newPrix > 0)
-    this.prix = newPrix;
-}
-*//*
-    //kanap.getHtmlRepresentation()
-getHtmlRepresentation() {
-        return       
-        `   <a href="/front/html/product.html?id=${this._id}">
-                <article>
-                    <img src="${this.imageUrl}" alt="${this.altTxt}" width="160" height="160">
-                    <h3 class="productName">${this.name}</h3>
-                    <p class="productDescription">${this.description}</p>
-                </article>
-            </a>
-      `;
-      
-}
-
-//kanap.htmlRepresentation
-
-get htmlRepresentation() {
-    return getHtmlRepresentation();
-}
-
-}
-
-*/
-
-
-
-//assign l'objet productList dans l'objet this sans écraser celles qui éxistes déjà 
-//this represente une instance de la classe Article
-
-/*La méthode Object.assign() est utilisée afin de copier les valeurs de toutes les propriétés directes (non héritées) d'un objet qui sont énumérables sur un autre objet cible. Cette méthode renvoie l'objet cible.
-*/
-
-
-/*Equivaut a faire 
-                            class Kanap {
-                                constructor(jsonArticle){
-                                    this.id = jsonArticle.id;
-                                    this.name = jsonArticle.name;
-                                    this.description = jsonArticle.description;
-                                    this.colors = jsonArticle.colors;
-                                    this.altText = jsonArticle.altText;
-                                 
-                                    
-                                }
-}*/
-/**
- *       `   <a href="/front/html/product.html?id=${kanap._id}">
-                                <article>
-                                    <img src="${kanap.imageUrl}" alt="${kanap.altTxt}" width="160" height="160">
-                                    <h3 class="productName">${kanap.name}</h3>
-                                    <p class="productDescription">${kanap.description}</p>
-                                </article>
-                              </a>
-                          `
- */
-                                     
+                          
                            
                     
--- front/js/script.js
+++ front/js/script.js
@@ -1,98 +1,35 @@
 import {Kanap} from "./Modelkanap.js";
 let urlApiAllkanaps = "http://localhost:3000/api/products";
 const items = document.querySelector("#items");
-/**
- * fetch API display all articles
- */
-const displayAllKanaps = async ()=> {
-    await fetch(urlApiAllkanaps)
-    .then(data => data.json())
-    .then(jsonListArticle => {   
-        for(let productList of jsonListArticle){
-                 let kanap = new Kanap(productList);
-                        items.innerHTML += `<a href="/front/html/product.html?id=${kanap._id}">
-                                                <article>
-                                                    <img src="${kanap.imageUrl}" alt="${kanap.altTxt}" width="160" height="160">
-                                                    <h3 class="productName">${kanap.name}</h3>
-                                                    <p class="productDescription">${kanap.description}</p>
-                                                </article>
-                                            </a>
-                                           `    
-        };
-    })
-    .catch((error)=>{
-               alert("Merci de recherger la page, une erreur est survenue !");
-    })
-
- };
- displayAllKanaps();
-
-        
-//kanap.array.forEach(let productList of jsonListArticle
-
-
-
-
 
 /**
  * fetch API display all articles
  */
-/*
+
  async function getJsonFromApi(url) {
-    await fetch(url)
+     return await fetch(url)
     .then(data => data.json())
     .catch((error)=>{
                alert("Merci de recherger la page, une erreur est survenue !");
-    });
-    
+    });  
  };
 
 function AddHtmlToElement(element, html) {
     element.innerHTML += html;
 };
 
-
 async function displayAllKanaps() {
     let jsonKanaps = await getJsonFromApi(urlApiAllkanaps);
     let modelKanaps = jsonKanaps.map((jsonKanap) => new Kanap(jsonKanap));
-    modelKanaps.array.foreach((modelKanap) => {
+    modelKanaps.forEach((modelKanap) => {
         AddHtmlToElement(items, modelKanap.getHtmlRepresentationDisplay);
     });
 }
 displayAllKanaps();
-*/
 
 
 
 
 
 
-/*
-function DisplayAllKanaps2() {
-    getJsonFromApi(urlAllKanaps)
-    .then((jsonKanaps) => jsonKanaps.map((jsonKanap) => new Kanap(jsonKanap)))
-    .then((modelKanaps) => modelKanaps.foreach((modelKanap) => AddHtmlToElement(items, modelKanap.getHtmlRepresentation())))
-    .catch((errrt) => {
 
-    })
-}
-*/
-/*
-
-function _CreateModelKanapsFromJson(jsonKanaps) {
-    return jsonKanaps.map((jsonKanap) => new Kanap(jsonKanap));
-}
-
-function _AddListKanapsToHtml(modelKanaps) {
-    return  modelKanaps.foreach((modelKanap) => AddHtmlToElement(items, modelKanap.getHtmlRepresentation()));
-}
-
-function DisplayAllKanaps3() {
-    getJsonFromApi(urlAllKanaps)
-    .then((jsonKanaps) => _CreateModelKanapsFromJson(jsonKanaps))
-    .then((modelKanaps) => _AddListKanapsToHtml(modelKanaps))
-    .catch((error) => {
-
-    })
-}
-*/
